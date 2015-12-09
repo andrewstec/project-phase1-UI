c06e5cd58a640141eca26ae5324507226c7dedf6
marrionluaka <davedsmithjr@gmail.com>
2015-12-08 16:01:10 -0800

    bootstrap styles

---
+++ organiclaunch_final/organiclaunch_final/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace organiclaunch_final.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+
+        public ActionResult Produce()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Meat()
+        {
+            return View();
+        }
+    }
+}
